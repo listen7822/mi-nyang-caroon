ed5510da509a6d69b46703f14301728f0533c4bd
losi8812 <j2bun@naver.com>
2019-06-06 20:43:33 +0900

    재료 움직이는 부분 구현

---
+++ Assets/ConveyorBeltManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltManager : MonoBehaviour
+{
+    private Transform m_Child;
+    private float m_velocity = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Child = gameObject.transform.Find("ConveyorBelt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Child.Translate(Vector2.left * m_velocity * Time.deltaTime, Space.Self);
+    }
+}
---
+++ Assets/IngredientsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientsManager : MonoBehaviour
+{
+    const int OFFSET = 10;
+    public GameObject m_Prefab;
+
+    private Vector2 m_Size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Size = GetComponent<RectTransform>().sizeDelta;
+        int prefabSizeOfX = (int)m_Prefab.GetComponent<RectTransform>().sizeDelta.x;
+        int count = Mathf.FloorToInt(m_Size.x / (prefabSizeOfX + OFFSET));
+        for (int i = 0; i < count; ++i)
+        {
+            GameObject child = GameObject.Instantiate(m_Prefab) as GameObject;
+            child.transform.position = new Vector2(i * (prefabSizeOfX + OFFSET), 0);
+            child.transform.SetParent(this.transform, false);
+        }
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+}
