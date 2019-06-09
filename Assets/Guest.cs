bfc0e79efe83dbcddba97c71223dc60fc42d4987
losi8812 <j2bun@naver.com>
2019-06-09 22:16:54 +0900

    Make convayor belt and tray

---
+++ Assets/Guest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guest : MonoBehaviour
+{
+    // 손님이 가져야하는 상태는?
+    // 밖에서 기다리는 상태
+    // 입구로 들어오는 상태
+    // 테이블까지 이동하는 상태
+    // 테이블에 앉은 상태
+    // 테이블에서 주문하는 상태
+    // 계산 및 만족도 표시 상태
+    // 입구로 나가는 상태
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/IngredientsManager.cs
+++ Assets/IngredientsManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class IngredientsManager : MonoBehaviour
 {
     const int OFFSET = 10;
+    const int MAX_COUNT = 7;
     public GameObject m_Prefab;
 
     private Vector2 m_Size;
@@ -14,11 +15,13 @@ public class IngredientsManager : MonoBehaviour
     {
         m_Size = GetComponent<RectTransform>().sizeDelta;
         int prefabSizeOfX = (int)m_Prefab.GetComponent<RectTransform>().sizeDelta.x;
-        int count = Mathf.FloorToInt(m_Size.x / (prefabSizeOfX + OFFSET));
-        for (int i = 0; i < count; ++i)
+        int tmp = Mathf.FloorToInt(m_Size.x / MAX_COUNT);
+        int tmp2 = tmp - prefabSizeOfX;
+        // int count = Mathf.FloorToInt(m_Size.x / (prefabSizeOfX + OFFSET));
+        for (int i = 0; i < MAX_COUNT; ++i)
         {
             GameObject child = GameObject.Instantiate(m_Prefab) as GameObject;
-            child.transform.position = new Vector2(i * (prefabSizeOfX + OFFSET), 0);
+            child.transform.position = new Vector2((i+1) * (prefabSizeOfX + (tmp2)), 0);
             child.transform.SetParent(this.transform, false);
         }
     } 
